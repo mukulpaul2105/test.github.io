b376e22aa2d8b5f1f24926dfdd3608f33b9b0b2e
Mukul Paul <94905659+mukulpaul2105@users.noreply.github.com>
2022-01-23 16:38:39 +0530

    Add files via upload

---
+++ Todo_list/Jan_23_todo_app.js
@@ -0,0 +1,18 @@
+function add_item() {
+  var item = document.getElementById("box");
+  var list_item = document.getElementById("list_item");
+  if (item.value != "") {
+    var make_li = document.createElement("LI");
+    make_li.appendChild(document.createTextNode(item.value));
+
+    list_item.appendChild(make_li);
+
+    item.value = "";
+
+    make_li.onclick = function () {
+      this.parentNode.removeChild(this);
+    };
+  } else {
+    alert("plz add a value to item");
+  }
+}
